841a3fc8d1ccbd4cb1095f08b6063a79c38267c8
Johnny Murillo <jossters@gmail.com>
2021-06-04 15:11:44 -0700

    heroku

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -20,7 +20,7 @@ router.get("/:id", validateProjectId, (req, res) => {
 router.post("/", validateProject, async (req, res, next) => {
   try {
     const newProject = await Projects.insert(req.body);
-    res.status(201).json(newProject);
+    res.json(newProject);
   } catch (err) {
     next(err);
   }
